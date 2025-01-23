4876da4af983257c7f8fe4a6173bf72c78607cf0
abhishek <abhiattri27@gmail.com>
2025-01-23 22:17:47 +0530

    add money test

--- test-jasmine/moneyTest.js
+++ test-jasmine/moneyTest.js
@@ -1,7 +1,15 @@
-import formatCurrency from "../scripts/utils.js/money";
+import formatCurrency from "../scripts/utils.js/money.js";
 
 describe("test suite: formatCurrency", ()=>{
     it('convert cents into dollars', ()=>{
-        expect(formatCurrency(2026)).isequal('20.26')
+        expect(formatCurrency(2026)).toEqual('20.26')
     });
-});
+
+    it('works with 0',()=>{
+        expect(formatCurrency(0)).toEqual('0.00')
+    });
+
+    it('round up to the nearest cent', ()=>{
+        expect(formatCurrency(2000.5)).toEqual('20.01')
+    });
+}); 
